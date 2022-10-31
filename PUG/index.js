b1eca033e899c2ab1d2e4f9be046c669741a92ac
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-10-30 21:50:47 -0300

    modularizing the project

--- PUG/index.js
+++ PUG/index.js
@@ -9,213 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(__dirname + '/public'));
 
-const server = app.listen(PORT, () => {
-    console.log(`Puerto ${server.address().port} 43495`);
-})
-
-// CLASS
-class Contenedor {
-    constructor(nameFile) {
-        this.nameFile = nameFile;
-    }
-
-    async save(ObjectToInsert) {
-        // Number - Receives an object, saves it to the file, returns the assigned id.
-
-        try {
-            const file = await fs.promises.readFile(this.nameFile, "utf-8")
-            let parsedFile = await JSON.parse(file)
-
-            ObjectToInsert["id"] = uuidv4();
-
-            await fs.promises.writeFile(this.nameFile, JSON.stringify(parsedFile = [...parsedFile, ObjectToInsert]), "utf-8")
-
-            console.log(ObjectToInsert["id"]);
-            return ObjectToInsert["id"]
-        } catch (error) {
-            if (error.code === "ENOENT") {
-                fs.writeFile(this.nameFile, "[]", (e) => {
-                    console.log("writeFile in save", e);
-                })
-            }
-            console.log("save", error);
-        }
-    }
-
-    async getById(Id) {
-        // ~ getById(Number): Object - Receives an id and returns the object with that id, or null if not present.
-        try {
-
-            const file = await fs.promises.readFile(this.nameFile, "utf-8")
-            let parsedFile = await JSON.parse(file)
-            let elementById
-
-            parsedFile.forEach(element => {
-                if (element.id == Id) {
-                    elementById = element
-                    return element
-                } else {
-                    return null
-                }
-            });
-
-            return elementById
-
-        } catch (error) {
-            console.log("getById()", error);
-        }
-
-    }
-
-    async deleteById(Id) {
-        // ~ deleteById(Number): void - Deletes the object with the searched id from the file.
-        try {
-
-            const file = await fs.promises.readFile(this.nameFile, "utf-8")
-            let parsedFile = await JSON.parse(file)
-
-            let positionObj
-            let elementToDelete
-            parsedFile.forEach(element => {
-                if (element.id == Id) {
-                    // console.log(element);     
-                    elementToDelete = element
-                    return parsedFile
-                } else {
-                    return null
-                }
-            });
-            positionObj = parsedFile.indexOf(elementToDelete)
-            console.log(parsedFile[positionObj]);
-            parsedFile.splice(positionObj, 1)
-
-
-            await fs.promises.writeFile(this.nameFile, JSON.stringify(parsedFile), "utf-8")
-            return parsedFile
-
-        } catch (error) {
-            console.log("getById()", error);
-        }
-    }
-
-    async updateById(id, title, price) {
-        const file = await fs.promises.readFile(this.nameFile, "utf-8")
-        let parsedFile = await JSON.parse(file)
-
-        let elementToUpdate
-        let indexElement
-        let finalElement
-
-        parsedFile.forEach(element => {
-            if (element.id == id) {
-                elementToUpdate = element
-                // console.log(element);
-            }
-        })
-        indexElement = parsedFile.indexOf(elementToUpdate)
-        console.log(parsedFile[indexElement]);
-        finalElement = parsedFile[indexElement]
-
-        if (title != undefined) {
-            finalElement.title = title
-            // console.log(finalElement);
-        }
-
-        if (price != undefined) {
-            finalElement.price = price
-            // console.log(finalElement);
-        }
-
-        await fs.promises.writeFile(this.nameFile, JSON.stringify(parsedFile), "utf-8")
-
-        return finalElement
-    }
-
-    async deleteById(id) {
-        const file = await fs.promises.readFile(this.nameFile, "utf-8")
-        let parsedFile = await JSON.parse(file)
-
-        let elementToDelete
-        let indexElement
-        let finalElementDelete
-        let deleted
-
-        parsedFile.forEach(element => {
-            if (element.id == id) {
-                elementToDelete = element
-                // console.log(element);
-            }
-        })
-
-        indexElement = parsedFile.indexOf(elementToDelete)
-        finalElementDelete = parsedFile[indexElement]
-        deleted = parsedFile.splice(indexElement, 1)
-        console.log("DELETED", deleted);
-
-        await fs.promises.writeFile(this.nameFile, JSON.stringify(parsedFile), "utf-8")
-
-        return deleted
-    }
-
-    async getAll() {
-        try {
-            const file = await fs.promises.readFile(this.nameFile, "utf-8")
-            let parsedFile = await JSON.parse(file)
-            return parsedFile
-        } catch (error) {
-            console.log("getAll()", error);
-        }
-    }
-
-    async deleteAll() {
-        try {
-
-            const file = await fs.promises.readFile(this.nameFile, "utf-8")
-            let parsedFile = await JSON.parse(file)
-
-            parsedFile.splice(0)
-
-            await fs.promises.writeFile(this.nameFile, JSON.stringify(parsedFile), "utf-8")
-
-            return parsedFile
-
-        } catch (error) {
-            console.log("deleteAll()", error);
-
-        }
-    }
-
-}
-
-
-const Escuadra = {
-    title: 'Escuadra',
-    price: 123.45,
-    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
-    id: 1
-}
-const Regla = {
-    title: 'Regla',
-    price: 123.45,
-    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
-    id: 1
-}
-
-const archivoDesafio = new Contenedor("./ejercicio.json")
-// archivoDesafio.save(Escuadra)
-// archivoDesafio.getById("67a4635f-b9c7-4f9e-a97f-7c1ffffa41ea")
-// archivoDesafio.getById("99949c2e-811d-4986-84d7-456959c5b3eb")
-// archivoDesafio.getAll()
-// archivoDesafio.deleteById("6f179a05-0840-467f-bd57-4499021839f0")
-// archivoDesafio.deleteAll()
-// CLASS
-
-
 // ROUTES
-
-
-
-
 app.get("/", (req, res, next) => {
     console.log("Principal Route");
     const principalRoute = {
@@ -227,7 +21,6 @@ app.get("/", (req, res, next) => {
     next()
 })
 
-app.use("/api/products/", apiProducts)
 
 //  GET RUTA PARA EL POST
 app.get("/form", (req, res) => {
@@ -235,71 +28,17 @@ app.get("/form", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
 })
 
-// GET /api/products/ - Return all the products
-apiProducts.get("/", async (req, res, next) => {
-
-    const syncProducts = await archivoDesafio.getAll()
-
-    res.json(syncProducts)
-
-    console.log("GET - Route: /api/products/");
-    next()
-})
-
-// GET /api/products/:id - Return the product specified by ID parameters
-apiProducts.get("/:id", async (req, res, next) => {
-    const { id } = req.params
-
-    const synGetById = await archivoDesafio.getById(id)
-
-    res.json(synGetById)
-
-    console.log("GET - Route: /api/products/:id");
-    next()
-})
-
-// POST - Receives and adds a product, and returns it with its assigned id.
-apiProducts.post("/", async (req, res, next) => {
-    const { body } = req
-    const elementSaved = await archivoDesafio.save(body)
-
-    console.log(elementSaved);
-    res.json(body)
-
-    console.log("POST - Route: /api/products/:id");
-    console.log("Element saved --> ", elementSaved);
-})
-
-// PUT /api/products/:id Receives an ID and update by ID.
-// http://localhost:8000/api/products/4c45bf45-d5ef-4d97-8332-592979ac63cd
-apiProducts.put("/:id", async (req, res, next) => {
-    const { id } = req.params
-    const { body } = req
-    const { title } = body
-    const { price } = body
-
-    const updateById = await archivoDesafio.updateById(id, title, price)
-
-    res.json(updateById)
-    console.log("PUT - Route /api/productos/:id ");
-})
-
-// DELETE /api/products/:id Receives an ID and delete by ID.
-// http://localhost:8000/api/products/4c45bf45-d5ef-4d97-8332-592979ac63cd
-
-apiProducts.delete("/:id", async (req, res) => {
-    const { id } = req.params
-
-    let deleteById = await archivoDesafio.deleteById(id)
-    let rtaFinal = {}
-
-        rtaFinal = {
-            success: true,
-            deleted: deleteById
-        }
-        res.json(rtaFinal)
+app.use("/api/products/", require("../router/routerApiProducts"))
+// ROUTES
 
 
+// VIEWS CONFIG 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Puerto ${server.address().port} 43495`);
 })
 
 
