afef22bb6f52d522ef1d1502a3bdb88ceecbd9a0
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-01-12 12:26:33 -0300

    No funciona auth en main, peor si en otras rutas

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 7070
 
 // COOKIES
-const session = require("express-session")
-const MongoStore = require("connect-mongo")
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 // COOKIES
 
 // Normalizr
-const { normalize, schema, denormalize } = require("normalizr")
+const { normalize, schema, denormalize } = require('normalizr')
 // Normalizr
 
 // SOCKET.IO
-const httpServer = require("http").createServer(app)
-const io = require("socket.io")(httpServer)
+const httpServer = require('http').createServer(app)
+const io = require('socket.io')(httpServer)
 // SOCKET.IO
 
 // Mongo CHAT
-const ChatMongo = require("./DAOS/Chat/ClassMongoChat.js")
-const schemaChat = require("./models/schemaChat.js")
+const ChatMongo = require('./DAOS/Chat/ClassMongoChat.js')
+const schemaChat = require('./models/schemaChat.js')
 const ChatMongoDB = new ChatMongo(schemaChat)
 // Mongo CHAT
 
-httpServer.listen(PORT, () => console.log("SERVER ON http://localhost:" + PORT))
+httpServer.listen(PORT, () => console.log('SERVER ON http://localhost:' + PORT))
 // Config
 // cookies
 app.use(
   session({
     store: MongoStore.create({
       mongoUrl:
-        "mongodb+srv://FUSSI:fussi0117@cluster0.jmg0aoz.mongodb.net/?retryWrites=true&w=majority",
+        'mongodb+srv://FUSSI:fussi0117@cluster0.jmg0aoz.mongodb.net/?retryWrites=true&w=majority',
       mongoOptions: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -38,109 +38,119 @@ app.use(
     }),
     cookie: { maxAge: 60000 * 10 },
 
-    secret: "secreto",
+    secret: 'secreto',
     resave: false,
     saveUninitialized: false,
   }),
 )
 // cookies
 app.use(express.json())
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({ extended: true }))
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
 // Config
 
 // ROUTER
-app.use("/api/products/", require("./Router/routerApiProducts.js"))
-app.use("/api/carrito/", require("./Router/routerApiCart.js"))
-app.use("/api/products-test/", require("./Router/routerFaker.js"))
+app.use('/api/products/', require('./Router/routerApiProducts.js'))
+app.use('/api/carrito/', require('./Router/routerApiCart.js'))
+app.use('/api/products-test/', require('./Router/routerFaker.js'))
 // ROUTER
 
 // fakerGenerator
-const generateURL = require("./FAKER/fakerGeneratorProds/fakerGeneratorProds.js")
+const generateURL = require('./FAKER/fakerGeneratorProds/fakerGeneratorProds.js')
 // fakerGenerator
 
 // percentageCalculator
-const percentageCalculator = require("./FAKER/percentageCalculator/percentageCalculator.js")
+const percentageCalculator = require('./FAKER/percentageCalculator/percentageCalculator.js')
 // percentageCalculator
 
 // MySQL Products
-const { PetitionKNEX } = require("./Classes/ClassKNEX") // CLASS KNEX
+const { PetitionKNEX } = require('./Classes/ClassKNEX') // CLASS KNEX
 
-const { optionsMySQL } = require("./options/options")
-const productsMySQL = new PetitionKNEX(optionsMySQL, "products")
+const { optionsMySQL } = require('./options/options')
+const productsMySQL = new PetitionKNEX(optionsMySQL, 'products')
 // productsMySQL.createTableProds() // This creates the table PRODUCTS
 
 // SQLite3 - Messages
-const { optionsSQLite3 } = require("./options/options")
-const chatSQLite3 = new PetitionKNEX(optionsSQLite3, "messages")
+const { optionsSQLite3 } = require('./options/options')
+const chatSQLite3 = new PetitionKNEX(optionsSQLite3, 'messages')
 // chatSQLite3.createTableChat() // This creates the table MESSAGES
 
 // Main PATH
 
-app.get("/", auth, (req, res) => {
-  res.redirect("pages/indexLogPOST.ejs", {
-    nameLoginn: req.session.user,
-    contrasenaLogin,
-  })
+app.get('/', auth, (req, res) => {
+  console.log(' ----------- / ----------- ')
+
+  res.render('pages/indexLogPOST.ejs', { nameLoginn: req.session.user })
 })
 
 function auth(req, res, next) {
-  console.log("AUUTH AUUUTH ")
-  if (req.session.user && req.session.user == "nombre") {
-    // res.render("pages/indexLog.ejs", {})
-    return next()
+  console.log('AAAAAAAAAAAAAAAAAAAAUTH')
+  if (req.session.user) {
+    console.log('AAAAAAAAAAAAAAAAAAAAUTH')
+    return next();
   } else {
-    return res.redirect("http://localhost:7070/login")
+    return res.redirect('/login')
   }
 }
 
-app.get("/login", (req, res) => {
-  console.log("LOGIN")
+/* function auth(req, res, next) {
+  console.log(' ------- AUUTH AUUUTH  ------- ')
+  if (req.session.user && req.session.user == 'nombre') {
+    // res.render("pages/indexLog.ejs", {})
+    return next()
+  } else {
+    return res.redirect('http://localhost:7070/login')
+  }
+} */
+
+app.get('/login', (req, res) => {
+  console.log(' ----------- LOGIN ----------- ')
   const { nameLogin, contrasenaLogin } = req.body
-  res.sendFile("/public/login.html", { root: __dirname })
+  res.sendFile('/public/login.html', { root: __dirname })
 })
 
-app.post("/", (req, res) => {
-  console.log("SIGN")
+app.post('/', (req, res) => {
+  console.log(' ----------- POST ----------- ')
 
   const { nameLogin, contrasenaLogin } = req.body
   console.log(nameLogin, contrasenaLogin)
-  if (nameLogin !== "nombre" && req.session.user !== "nombre") {
+  if (nameLogin !== 'nombre' && req.session.user !== 'nombre') {
     console.log(req.session.user)
-    return res.send("login failed")
+    return res.send('login failed')
   } else {
     req.session.user = nameLogin
     req.session.admin = true
   }
-  console.log("req.session", req.session)
-  console.log(" req.session.user", req.session.user)
+  console.log('req.session', req.session)
+  console.log(' req.session.user', req.session.user)
   // res.json({ nameLogin, contrasenaLogin })
-  res.render("pages/indexLogPOST.ejs", {
+  res.render('pages/indexLogPOST.ejs', {
     nameLoginn: req.session.user,
     contrasenaLogin,
   })
+  
 })
 
-app.get("/logout", (req, res) => {
-  console.log("LOGOUT")
+app.get('/logout', (req, res) => {
+  console.log(' ----------- LOGOUT ----------- ')
   const { user } = req.session
   req.session.destroy((err) => {
     if (err) {
       // res.send("no pudo deslogear");
-      res.render("pages/logout.ejs", { content: "No se pudo desloguear" })
+      res.render('pages/logout.ejs', { content: 'No se pudo desloguear' })
     } else {
-      // res.send("borramos todos quedate tranquilo que ya estas deslogeado")
-      res.render("pages/logout.ejs", {
-        content: "ya estas deslogueado: ",
+      res.render('pages/logout.ejs', {
+        content: 'ya estas deslogueado: ',
         user,
       })
     }
   })
 })
 
-app.get("/showsession", auth, (req, res) => {
+app.get('/showsession', auth, (req, res) => {
+  console.log(' ----------- SHOWSESSION ----------- ')
   res.json(req.session)
 })
 
@@ -172,14 +182,14 @@ async function normalizarMensajes() {
 }
 // normalizarMensajes
 
-io.on("connection", async (socket) => {
+io.on('connection', async (socket) => {
   //  ---- NORMALIZR ---- NORMALIZR ----
   let chatNormalized = await normalizarMensajes()
-  const authorSchema = new schema.Entity("authors", { idAttribute: "id" })
+  const authorSchema = new schema.Entity('authors', { idAttribute: 'id' })
   const messageSchema = new schema.Entity(
-    "message",
+    'message',
     { author: authorSchema },
-    { idAttribute: "_id" },
+    { idAttribute: '_id' },
   )
 
   const FINALchatNormalized = normalize(chatNormalized, [messageSchema])
@@ -199,36 +209,36 @@ io.on("connection", async (socket) => {
   const respuesta = [FINALchatNormalized, percetageNrmld]
   //  ---- NORMALIZR ---- NORMALIZR ----
 
-  console.log("SOCKET CONECTADO")
-  io.sockets.emit("chatPage", await respuesta)
+  console.log('SOCKET CONECTADO')
+  io.sockets.emit('chatPage', await respuesta)
 
-  socket.on("testChat", async (data) => {
+  socket.on('testChat', async (data) => {
     await ChatMongoDB.save(data)
     chatNormalized = await normalizarMensajes()
     const FINALchatNormalized = normalize(chatNormalized, [messageSchema])
     const respuesta = [FINALchatNormalized]
-    io.sockets.emit("chatPage", await respuesta)
+    io.sockets.emit('chatPage', await respuesta)
   })
 
   // ------- PRODUCTS SOCKET --------
-  let syncProductsMySQL = await productsMySQL.select("*")
+  let syncProductsMySQL = await productsMySQL.select('*')
 
-  socket.emit("products", syncProductsMySQL)
+  socket.emit('products', syncProductsMySQL)
 
-  socket.on("products", async (dataProds) => {
+  socket.on('products', async (dataProds) => {
     await productsMySQL.insert(dataProds)
 
-    let newSyncProductsMySQL = await productsMySQL.select("*")
+    let newSyncProductsMySQL = await productsMySQL.select('*')
 
-    io.sockets.emit("products", newSyncProductsMySQL)
+    io.sockets.emit('products', newSyncProductsMySQL)
   })
   // ------- PRODUCTS SOCKET --------
 
   // ----------- FAKER - NORMALIZR -----------
-  io.sockets.emit("prodsDesafio11", generateURL())
+  io.sockets.emit('prodsDesafio11', generateURL())
 
-  socket.on("prodsDesafio11", async (dataProds) => {
-    io.sockets.emit("prodsDesafio11 FAKER", generateURL())
+  socket.on('prodsDesafio11', async (dataProds) => {
+    io.sockets.emit('prodsDesafio11 FAKER', generateURL())
   })
   // ----------- FAKER - NORMALIZR -----------
 })
