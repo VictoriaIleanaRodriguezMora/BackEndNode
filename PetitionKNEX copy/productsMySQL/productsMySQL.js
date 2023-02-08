8014d818401c2953a99e2d56af210015dfd61b5e
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-02-08 12:33:37 -0300

    proyecto restaurado

--- PetitionKNEX copy/productsMySQL/productsMySQL.js
+++
@@ -1,5 +0,0 @@
-const { PetitionKNEX } = require('../petitionKNEX')
-const { optionsMySQL } = require('../../options/options')
-const productsMySQL = new PetitionKNEX(optionsMySQL, 'products')
-// productsMySQL.createTableProds() // This creates the table PRODUCTS
-module.exports = { productsMySQL }
