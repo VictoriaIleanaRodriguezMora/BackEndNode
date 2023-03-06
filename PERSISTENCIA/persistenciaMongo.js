8a21562f3fb49d890229d2ba6b2f1d56504bb65e
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-03-06 16:04:55 -0300

    .

---
+++ PERSISTENCIA/persistenciaMongo.js
@@ -0,0 +1,26 @@
+const {
+
+    UsuarioSchemaApp,
+    MongoUsersInstance,
+    CarritosSchema,
+    MongoCarritosInstance,
+    schemaUsuariosPassport
+
+} = require("../SERVICIO/servicioSchemas")
+
+
+
+async function findByUserName(username) {
+    const userFound = await MongoUsersInstance.getByUsername(username)
+
+    return userFound
+}
+
+async function saveCart(toSave) {
+    MongoCarritosInstance.saveCart(toSave)
+}
+
+module.exports = {
+    findByUserName,
+    saveCart
+}
---
+++ SERVICIO/servicioPassport.js
@@ -0,0 +1,23 @@
+const {
+    MongoUsersInstance,
+    MongoCarritosInstance
+} = require("./servicioSchemas.js")
+
+async function LoginRoot__ProfileUser__PassportService(req, res) {
+    const { username, password } = await req.user;
+    const user = { username, password };
+    return await res.render("./pages/profileUser", { user });
+}
+
+async function SignUp__ProfileUser__PassportService(req, res) {
+    const { username, password } = await req.user;
+    const { phone, adress, age, avatar, gmail } = await req.body
+    const user = { username, password, phone, adress, age, avatar, gmail };
+    await MongoUsersInstance.saveUser(user)
+    return await res.render("./pages/profileUser", { user });
+}
+
+module.exports = {
+    LoginRoot__ProfileUser__PassportService,
+    SignUp__ProfileUser__PassportService
+}
