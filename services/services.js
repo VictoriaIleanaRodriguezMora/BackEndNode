a944e63d8b34ad29bd3e71548dc2f4dd15f2167e
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-03-06 14:30:06 -0300

    Modo por consola

--- services/services.js
+++ services/services.js
@@ -1,9 +1,9 @@
-const { DAO__Final } = require("../models/schemaUsuariosApp")
+const { DAO } = require("../models/DAOs/main__daos")
 /* al negocio/servicio no le tiene que importar la bdd */
 
 async function getUsuariosService() {
     // este find, no va a funcionar aunque cambies solo el modelo a otra bd, pq eses find no va a existir.
-    const usuarios = await DAO__Final.getUsuariosModel()
+    const usuarios = await DAO.getUsuariosModel()
     return usuarios
 }
 
