a66fe755813c846eba40266abf1330ed94c0c88f
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-03-09 10:51:53 -0300

    guille repo

--- SERVICIO/services.js
+++
@@ -1,8 +0,0 @@
-const { DAO__Prods } = require("../models/DAOs/main__daos")
-
-async function getUsuariosService() {
-    const usuarios = await DAO__Prods.getAll()
-    return usuarios
-}
-
-module.exports = { getUsuariosService }
--- models/DAOs/Chat/MongoChat.js
+++
@@ -1,8 +0,0 @@
-const { DAO__Mongo } = require("../Containers/DAO__Mongo")
-
-class ChatDaoMongo extends DAO__Mongo {
-
-}
-
-
-module.exports = { ChatDaoMongo }
