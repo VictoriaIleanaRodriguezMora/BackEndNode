ef210b2fc1fd2e736198562c1c8d6f0c5901bab9
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-03-25 13:05:47 -0300

    cargar archivos

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
-const Initialize__App = require("./app")
-const myApp = new Initialize__App()
-myApp.listen() 
+const express = require('express')
+const multer = require("multer")
+const app = express()
+const port = 7000
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+})
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname +
+            '-' +
+            Date.now() +
+            '.' +
+            file.originalname.split('.').pop()
+        );
+    },
+});
+const upload = multer({ storage: storage });
+
