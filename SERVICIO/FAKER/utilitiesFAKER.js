d4d7cedc814d3fe50a53fdbdadf211616c7ad3e9
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2023-04-04 15:36:51 -0300

    deno

--- SERVICIO/FAKER/utilitiesFAKER.js
+++
@@ -1,6 +0,0 @@
-const { generateURL } = require("./GeneratorUrl")
-const { percentageCalculator } = require("./percentageCalculator")
-const { generateCarts } = require("./GeneratorCarts")
-const { generateProds } = require("./GeneratorProds")
-
-module.exports = { percentageCalculator, generateURL, generateCarts, generateProds }
